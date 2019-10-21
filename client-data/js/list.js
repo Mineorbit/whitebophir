047c104dafc5a2e7cfbad4514043c004211d3b4e
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-10-21 21:24:56 +0200

    improve js

--- client-data/js/list.js
+++ client-data/js/list.js
@@ -1,16 +1,9 @@
-
-		window.onload = function () {
-		var xmlHttp = new XMLHttpRequest();
-    		xmlHttp.open( "GET", "/list", false ); // false for synchronous request
-    		xmlHttp.send( null );
-   	 	var res = xmlHttp.responseText.split("\n");
-		document.getElementById("list").innerHTML += "<ul>";
-		
-		for(i = 1;i<res.length;i++)
-		{
-		
+window.onload = function () {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", "/list", false ); // false for synchronous request
+	xmlHttp.send( null );
+	var res = xmlHttp.responseText.split("\n");
+	for(i = 1;i<res.length;i++) {
 		document.getElementById("list").innerHTML += "<li><a href='/boards/"+res[i]+"'>"+res[i]+"</a></li>";
-		}
-			
-		document.getElementById("list").innerHTML += "</ul>";
-		};
+	}
+};
