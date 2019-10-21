fdc69bc56d76e18aa0de236b23c342c791dac577
mineorbit <maximilian.stillger@arcor.de>
2019-10-21 13:51:31 +0200

    list.js

---
+++ client-data/list.js
@@ -0,0 +1,11 @@
+
+		var xmlHttp = new XMLHttpRequest();
+    		xmlHttp.open( "GET", "/list", false ); // false for synchronous request
+    		xmlHttp.send( null );
+   	 	var res = xmlHttp.responseText.split("\n");
+		
+		document.getElementById("Boards").innerHTML = "Test";
+		for(i = 1;i<res.length;i++)
+		{
+		document.getElementById("list").innerHTML += "\n<a href='/boards/"+res[i]+"'>"+res[i]+"</a>";
+		}
