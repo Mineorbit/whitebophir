14211b01aff25a7953b4bd12947d85c305ed7852
mineorbit <maximilian.stillger@arcor.de>
2019-10-21 14:17:57 +0200

    so fertig

--- client-data/js/list.js
+++ client-data/js/list.js
@@ -4,11 +4,13 @@
     		xmlHttp.open( "GET", "/list", false ); // false for synchronous request
     		xmlHttp.send( null );
    	 	var res = xmlHttp.responseText.split("\n");
+		document.getElementById("list").innerHTML += "<ul>";
 		
-
-		document.getElementById("list").innerHTML = "Test";
 		for(i = 1;i<res.length;i++)
 		{
-		document.getElementById("list").innerHTML += "\n<a href='/boards/"+res[i]+"'>"+res[i]+"</a>";
+		
+		document.getElementById("list").innerHTML += "<li><a href='/boards/"+res[i]+"'>"+res[i]+"</a></li>";
 		}
+			
+		document.getElementById("list").innerHTML += "</ul>";
 		};
